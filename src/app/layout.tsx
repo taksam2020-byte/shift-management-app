3f5a9a6130e195980a33b35e30d6468677d148e0
unknown <ueki@taksam.co.jp>
2025-09-17 10:39:50 +0900

    Fix: ビルドエラーを修正 (Import)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import AuthProvider from "@/components/AuthProvider";
+import { AuthProvider } from "@/components/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
